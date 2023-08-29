5ef27fad71e03c6accf22c85e0771c4307073cc2
Anupana Udeshani <anubwabt414@gmail.com>
2023-08-29 21:18:29 +0530

    create folders

---
+++ mini/src/component/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import  '../css/Sidebar.css';
+
+export default function Sidebar() {
+  return (
+    <div class='Sidebarstyle'>
+        <h1 class='admintext'>Admin</h1>
+        <div class='logoutbox'>
+             
+            <h1 class='logout'>Logout</h1>
+        </div>
+    </div>
+  )
+}
---
+++ mini/src/pages/Loginpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Loginpage() {
+  return (
+    <div>
+      <div class='loginimage'>
+
+      </div>
+    
+    </div>
+  )
+}
