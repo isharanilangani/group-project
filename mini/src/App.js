25ce52193745974029051a3292494de1997fec81
Anupana Udeshani <anubwabt414@gmail.com>
2023-09-11 20:14:13 +0530

    create Navigation bar

--- mini/src/App.js
+++ mini/src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
 
-//import Notification from './component/Notification';
-import Loginpage from './pages/Loginpage';
+import Navigation from './component/Navigationbar';
+//import Loginpage from './pages/Loginpage';
 //import Bot_request from './pages/Bot_request';
 
 //import Sidebarcode from './component/Sidebar';
@@ -10,7 +10,8 @@ import Loginpage from './pages/Loginpage';
 function App() {
   return (
     <div className="App">
-     <Loginpage/>
+     
+     <Navigation/>
     </div>
   );
 }
---
+++ mini/src/component/Navigationbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../css/Navigationbar.css';
+import Searchicon from '../images/Searchicon.svg';
+import liblogo from '../images/logo.svg';
+
+export default function () {
+  return (
+    <div class='navigation'>
+      <img src={Searchicon} alt='search' class='Searchicon'/>
+      <h1 class='topic'>Welcome ChatLib</h1> 
+      <img src={liblogo} alt='logo' class='logo'/>
+
+       
+     
+    </div>
+  )
+}
+
