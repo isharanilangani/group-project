6a188e3456053358d5c6625a0f6f7f73f26ae880
Anupana Udeshani <anubwabt414@gmail.com>
2023-09-13 08:23:23 +0530

    modify bot_request page

--- mini/src/component/Navigationbar.jsx
+++ mini/src/component/Navigationbar.jsx
@@ -3,15 +3,12 @@ import '../css/Navigationbar.css';
 import Searchicon from '../images/Searchicon.svg';
 import liblogo from '../images/logo.svg';
 
-export default function () {
+export default function  Navigationbar() {
   return (
     <div class='navigation'>
       <img src={Searchicon} alt='search' class='Searchicon'/>
       <h1 class='topic'>Welcome ChatLib</h1> 
-      <img src={liblogo} alt='logo' class='logo'/>
-
-       
-     
+      <img src={liblogo} alt='logo' class='logo'/>  
     </div>
   )
 }
--- mini/src/component/Sidebar.jsx
+++ mini/src/component/Sidebar.jsx
@@ -11,30 +11,30 @@ export default function Sidebar() {
   return (
     <div class='Sidebarstyle'>
       <p class='admin'>Admin</p>
-      <div class='botrequestbox'>
+      <button class='botrequestbox'>
        <img src={inquiry} alt='search' class='botrequesticon'/>
        <p class='botrequest'>Bot requests</p>
-      </div>
-      <div class='booksbox'>
+      </button>
+      <button class='booksbox'>
       <img src={assign} alt='book' class='bookicon'/>
        <p class='books'>Books</p>
-      </div>
-      <div class='userbox'>
+      </button>
+      <button class='userbox'>
       <img src={account} alt='user' class='usericon'/>
        <p class='users'>Users</p>
-      </div>
-      <div class='checkbox'>
+      </button>
+      <button class='checkbox'>
       <img src={groups} alt='check' class='checkicon'/>
        <p class='checkinout'>Check In / Out</p>
-      </div>
-      <div class='settingbox'>
+      </button>
+      <button class='settingbox'>
       <img src={setting} alt='setting' class='settingicon'/>
        <p class='settings'>Settings</p>
-      </div>
-      <div class='logoutbox'>
+      </button>
+      <button class='logoutbox'>
       <img src={shutdown} alt='logout' class='logouticon'/>
        <p class='logout'>Logout</p>
-      </div>
+      </button>
         
     </div>
   )
